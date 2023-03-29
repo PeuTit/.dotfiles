f27c1becfe5f1a510c082825b0de35e1f4b3c8ab
Titouan Guiochet <22703571+PeuTit@users.noreply.github.com>
2023-03-29 16:09:58 +0200

    Add keybinds to change tabs

--- nvim/.config/nvim/lua/keys.lua
+++ nvim/.config/nvim/lua/keys.lua
@@ -1,3 +1,10 @@
 local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
 
-map('n', 'n', [[:NvimTreeToggle]], {})
+-- Tree
+map('n', 'n', '<Cmd>NvimTreeToggle<CR>', {})
+
+-- Bar
+map('n', '<A-,>', '<Cmd>tabprevious<CR>', opts)
+map('n', '<A-.>', '<Cmd>tabnext<CR>', opts)
+
