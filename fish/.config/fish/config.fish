b37fa1b1769446e2907e82715eb6093aeca11e11
Titouan Guiochet <22703571+PeuTit@users.noreply.github.com>
2023-05-01 20:20:33 +0200

    fish - inital configuration file
    Minimal fish configuration file that covers all of my needs.
    Perfect replacement for my previous setup with zsh & oh-my-zsh.
    
    - Set most used git aliases
    - Set global alias for listing files & folders
    - Set vi mode in shell with different cursors
    - Set tty for entering gpg passphrase during commits

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,27 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    # Git
+    abbr -a --global -- gst 'git status'
+    abbr -a --global -- ga 'git add'
+    abbr -a --global -- gaa 'git add --all'
+    abbr -a --global -- gc 'git commit -S -v'
+    abbr -a --global -- glo 'git log --decorate --oneline'
+    abbr -a --global -- gp 'git push'
+    abbr -a --global -- gl 'git pull'
+    abbr -a --global -- gb 'git branch'
+    abbr -a --global -- gs 'git switch'
+
+    abbr -a --global -- l 'ls -la'
+
+    fish_vi_key_bindings insert
+    set fish_cursor_default block
+    set fish_cursor_insert line
+    set fish_cursor_replace_one underscore
+end
+
+# todo:
+# create a quick way to toggle between light & dark mode in Neovim
+alias lnvim="nvim --cmd \"let g:light='true'\""
+
+# use to enter pgp passphrase when commiting
+set -gx GPG_TTY "$(tty)"
