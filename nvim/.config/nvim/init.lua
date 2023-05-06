cb2c0c03b161adf932eb1e8174ffbc085851148e
Titouan Guiochet <22703571+PeuTit@users.noreply.github.com>
2023-05-06 22:36:36 +0200

    nvim - metals configuration
    Pass the metal_config to the on_attach function.
    Define `*.scala.html` file as scala file.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -299,16 +299,28 @@ cmp.setup({
 -- metals configuration
 local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
 
+local metals_config = require("metals").bare_config()
+metals_config.init_options.statusBarProvider = "on"
+
+metals_config.on_attach = function(client, bufnr)
+  on_attach(client, bufnr)
+end
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "scala", "sbt", "java" },
   callback = function()
-    require("metals").initialize_or_attach({})
+    require("metals").initialize_or_attach(metals_config)
   end,
   group = nvim_metals_group,
 })
 
-local metals_config = require("metals").bare_config()
-metals_config.init_options.statusBarProvider = "on"
+
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.scala.html" },
+  command = "set filetype=scala",
+  group = nvim_metals_group,
+})
 
 --Set completeopt to have a better completion experience
 -- :help completeopt
