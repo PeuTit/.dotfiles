dec94711ed02dde8178166196d1129c9bb45a429
Titouan Guiochet <22703571+PeuTit@users.noreply.github.com>
2025-02-21 16:55:39 +0100

    nvim - treesitter configuration

--- nvim/.config/nvim/lua/crozes-hermitage/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/crozes-hermitage/plugins/treesitter.lua
@@ -1,20 +1,27 @@
+---@diagnostic disable: missing-fields
 return {
-  'nvim-treesitter/nvim-treesitter',
-  config = function()
-    local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-    ts_update()
-
-    require('nvim-treesitter.configs').setup {
-      modules = {},
-      sync_install = false,
-      ignore_install = {},
-      ensure_installed = { 'c', 'lua', 'rust', 'vimdoc', 'vim', 'scala' },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = { enable = true },
-    }
-  end
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      require 'nvim-treesitter.configs'.setup {
+        -- A list of parser names, or 'all' (the listed parsers MUST always be installed)
+        ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline', 'scala' },
+        auto_install = false,
+        highlight = {
+          enable = true,
+          ---@diagnostic disable-next-line: unused-local
+          disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            ---@diagnostic disable-next-line: undefined-field
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
+          additional_vim_regex_highlighting = false,
+        },
+      }
+    end
+  }
 }
