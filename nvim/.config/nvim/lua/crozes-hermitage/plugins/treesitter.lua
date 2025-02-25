6dde1ff56bcca305bbe8176a172445ba48c98439
Titouan Guiochet <22703571+PeuTit@users.noreply.github.com>
2025-02-25 21:36:37 +0100

    nvim - add groovy parser

--- nvim/.config/nvim/lua/crozes-hermitage/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/crozes-hermitage/plugins/treesitter.lua
@@ -5,8 +5,7 @@ return {
     build = ':TSUpdate',
     config = function()
       require 'nvim-treesitter.configs'.setup {
-        -- A list of parser names, or 'all' (the listed parsers MUST always be installed)
-        ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline', 'scala' },
+        ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline', 'scala', 'groovy' },
         auto_install = false,
         highlight = {
           enable = true,
