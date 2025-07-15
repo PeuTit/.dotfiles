a1479662bb2e324b400c72cabd5808922e68a98f
PeuTit <22703571+PeuTit@users.noreply.github.com>
2025-07-15 16:38:42 +0200

    nvim(git): remove git-conflict, add git blame toggle

--- nvim/.config/nvim/lua/crozes-hermitage/plugins/git.lua
+++ nvim/.config/nvim/lua/crozes-hermitage/plugins/git.lua
@@ -1,14 +1,13 @@
 local map = require('crozes-hermitage.utils').map
+
 return {
   -- Git signs in the gutter (left to the numbers)
-  { 'lewis6991/gitsigns.nvim', opts = {} },
-  -- Git conflict
   {
-    'akinsho/git-conflict.nvim',
-    version = "*",
+    'lewis6991/gitsigns.nvim',
     opts = {},
     config = function()
-      map('n', '<leader>glc', ':GitConflictListQf<cr>', { desc = '[G]it [L]ist [C]onflict' })
+      map('n', '<leader>tb', ':Gitsigns toggle_current_line_blame<CR>',
+        { desc = '[T]oggle Git [B]lame' })
     end
   },
 }
